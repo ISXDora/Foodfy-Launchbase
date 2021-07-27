c09fa4fa35eb5642639d7b65d943d12d09dfc4ac
ISXDora <isinhasena@gmail.com>
2021-07-27 11:23:01 -0300

    Modal abre e fecha com JS

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const recipes = document.querySelectorAll('.recipe-content');
+
+for (let recipe of recipes){
+    recipe.addEventListener("click", function(){
+        modalOverlay.classList.add('active')
+    })
+}
+
+
+document.querySelector('.close-modal').addEventListener("click", function(){
+    modalOverlay.classList.remove('active')
+})
+
