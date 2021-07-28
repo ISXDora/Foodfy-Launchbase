bf9a68d50956259b1dd3d6753b7e29345e8284fc
ISXDora <isinhasena@gmail.com>
2021-07-28 15:22:04 -0300

    Recebendo dados no modal rodando redondo

--- scripts.js
+++ scripts.js
@@ -1,14 +1,24 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const recipes = document.querySelectorAll('.recipe-content');
+const modal = document.querySelector('.modal');
 
 for (let recipe of recipes){
     recipe.addEventListener("click", function(){
-        modalOverlay.classList.add('active')
+        const imgId = recipe.getAttribute("id");
+        const tituloModal = recipe.querySelector("h3").textContent;
+        const autorModal = recipe.querySelector('p').textContent;
+        modalOverlay.classList.add('active');
+        modalOverlay.querySelector('img').src =`/assets/${imgId}`
+        modalOverlay.querySelector('h3').textContent = `${tituloModal}`
+        modalOverlay.querySelector('p').textContent = `${autorModal}`
+
     })
 }
 
 
 document.querySelector('.close-modal').addEventListener("click", function(){
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('img').src =""
 })
+
 
