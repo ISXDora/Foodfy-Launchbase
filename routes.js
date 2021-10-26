698d70486f5b1332ae431306de9fd1c2d66ebfef
ISXDora <isinhasena@gmail.com>
2021-10-25 21:57:42 -0300

    starting view create

--- routes.js
+++ routes.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const routes = express.Router()
-const recipes = require('./data.js')
-const admin = require('./controllers/admin')
+const recipesData = require('./data.js')
+const recipes = require('./controllers/recipes')
 
 
 
 
 
 routes.get('/', (req,res) =>{
-    return  res.render('website/index', { items: recipes })
+    return  res.render('website/index', { items: recipesData })
  })
 routes.get('/about', (req,res) =>{
      return  res.render('website/about')
   })
 routes.get('/recipes', (req,res) =>{
-     return  res.render('website/recipes', {items: recipes})
+     return  res.render('website/recipes', {items: recipesData})
   })
 routes.get('/recipes/:index', (req,res) =>{
      
      const indexRecipe = req.params.index
-     const recipe = recipes
+     const recipe = recipesData
      console.log(recipe[indexRecipe.replace(":","")])
  
      return res.render("website/recipe", {items: recipe[indexRecipe.replace(":", "")]})
@@ -27,9 +27,13 @@ routes.get('/recipes/:index', (req,res) =>{
  }) 
 
 
- routes.get("/admin/recipes", admin.index);
+ routes.get("/admin", (req, res) => {
+    return res.redirect("/admin/recipes")
+ })
 
-//routes.get("/admin//create", recipes.create); // Mostrar formulário de nova receita
+ routes.get("/admin/recipes", recipes.index);
+
+ routes.get("/admin/create", recipes.create); // Mostrar formulário de nova receita
 //routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
 //routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
