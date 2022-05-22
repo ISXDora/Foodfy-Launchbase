6d02797516baf9715011136a8b2531206b4a75a6
Isadora Xavier <isinhasena@gmail.com>
2022-05-22 15:30:02 -0300

    refactor: após mini gerenciador de img no front

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const multer = require('./app/middlewares/multer')
 const recipes = require('./app/controllers/admin/recipes')
 const chefs = require('./app/controllers/admin/chefs')
 const data = require('./app/controllers/website/recipes')
@@ -22,8 +23,8 @@ routes.get('/recipes/:id', data.show)
  routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
  routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
  routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
- routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
- routes.put("/admin/recipes", recipes.put); // Editar uma receita
+ routes.post("/admin/recipes", multer.array("photos", 5), recipes.post); // Cadastrar nova receita
+ routes.put("/admin/recipes", multer.array("photos", 5),recipes.put); // Editar uma receita
  routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
  
 
@@ -32,8 +33,8 @@ routes.get('/recipes/:id', data.show)
  routes.get("/admin/chefs/create", chefs.create); // Mostrar formulário de nova receita
  routes.get("/admin/chefs/:id", chefs.show); // Exibir detalhes de uma receita
  routes.get("/admin/chefs/:id/edit", chefs.edit); // Mostrar formulário de edição de receita
- routes.post("/admin/chefs", chefs.post); // Cadastrar nova receita
- routes.put("/admin/chefs", chefs.put); // Editar uma receita
+ routes.post("/admin/chefs", multer.single("photo-chefs"),chefs.post); // Cadastrar nova receita
+ routes.put("/admin/chefs", multer.array("photos",5),chefs.put); // Editar uma receita
  routes.delete("/admin/chefs", chefs.delete);
 
  module.exports = routes
